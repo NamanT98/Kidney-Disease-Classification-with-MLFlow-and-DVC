c2b6cb55c800d56714f7823f663c79355448e801
NamanT98 <namant@iitbhilai.ac.in>
2024-10-25 11:16:02 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Kidney-Disease-Classification-with-MLFlow-and-DVC"
+AUTHOR_USER_NAME = "NamanT98"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "namant@iitbhilai.ac.in"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A machine learning project for predicting kidney disease using Convolutional Neural Networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
